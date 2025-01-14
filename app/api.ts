e11662b77ca797c9706f9cd2b29da0befa6f3e22
joaovmour4 <joaov.mour@gmail.com>
2025-01-14 11:48:29 -0300

    integrating with api

---
+++ app/api.ts
@@ -0,0 +1,33 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import axios from "axios"
+
+const api = axios.create({
+  baseURL: `http://192.168.1.11:8080`,
+});
+
+api.interceptors.request.use(
+  async config => {
+    const token = await AsyncStorage.getItem('userToken')
+    if(token)
+      config.headers.Authorization = `Bearer ${token}`
+    return config
+  },
+  error => {
+    return Promise.reject(error)
+  }
+)
+
+api.interceptors.response.use(
+  (response) => {
+    return response
+  },
+  (error) => {
+    if(error.response && error.response.status === 401){
+      AsyncStorage.removeItem('userToken')
+      AsyncStorage.removeItem('user')
+    }
+    return Promise.reject(error)
+  }
+)
+
+export default api;
--- app/login.tsx
+++ app/login.tsx
@@ -1,7 +1,42 @@
-import { Image, ImageBackground, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
+import { Alert, Image, ImageBackground, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
 import React from 'react'
+import api from './api'
+import { jwtDecode } from 'jwt-decode'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { useRouter } from 'expo-router'
+import axios from 'axios'
 
 export default function login() {
+    const router = useRouter()
+    const [login, setLogin] = React.useState<string>("")
+    const [passsword, setPassword] = React.useState<string>("")
+
+    const loginFn = () => {
+        api
+            .post('/auth/login', {
+                name: login,
+                password: passsword
+            })
+            .then(response => {
+                const token = jwtDecode(response.data)
+                AsyncStorage.setItem('userToken', response.data)
+                AsyncStorage.setItem('user', JSON.stringify(token.sub))
+                router.navigate('/(tabs)')
+            })
+            .catch(error => {
+                Alert.alert(
+                    "Erro de Login",
+                    "Dados de login incorretos. Tente novamente.",
+                    [
+                        {
+                            text: "OK", // Texto do botão
+                            onPress: () => console.log("OK Pressed"), // Ação ao pressionar o botão
+                        },
+                    ],
+                )
+            })
+    }
+
     return (
         <View style={styles.container}>
             <Image 
@@ -11,19 +46,24 @@ export default function login() {
             <View style={styles.centralView}>
                 <View style={styles.inputView}>
                     <TextInput 
+                        value={login}
+                        onChangeText={setLogin}
                         style={styles.input} 
                         placeholder='USUÁRIO'
                         placeholderTextColor={'#4A2C1A'}
                     />
                     <TextInput 
+                        value={passsword}
+                        onChangeText={setPassword}
                         style={styles.input} 
                         placeholder='SENHA' 
                         placeholderTextColor={'#4A2C1A'}
                         textContentType='password'
+                        secureTextEntry
                     />
                 </View>
                 <View style={styles.buttonView}>
-                    <TouchableOpacity style={styles.signInButton}>
+                    <TouchableOpacity style={styles.signInButton} onPress={loginFn}>
                         <Text style={styles.signInText}>
                             ENTRAR
                         </Text>
@@ -56,15 +96,21 @@ const styles = StyleSheet.create({
         flex: 1,
         maxHeight: '50%',
         alignContent: 'center',
-        justifyContent: 'space-between',
         backgroundColor: '#6E8B3D',
         paddingInline: 15,
         paddingBlock: 25,
         marginHorizontal: 25,
         borderRadius: 15,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.2,
+        shadowRadius: 3,
+        elevation: 10,
     },
     inputView: {
         gap: 15,
+        flex: 1,
+        justifyContent: 'center'
     },
     input: {
         color: '#4A2C1A',
@@ -74,10 +120,17 @@ const styles = StyleSheet.create({
         height: 55,
         fontSize: 16,
         borderRadius: 15,
-        paddingLeft: 30
+        paddingLeft: 30,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.2,
+        shadowRadius: 3,
+        // Sombra para Android
+        elevation: 10,
     },
     buttonView: {
-        gap: 15
+        gap: 15,
+        flex: 0
     },
     signInButton: {
         backgroundColor: '#FFFFFF',
@@ -96,14 +149,14 @@ const styles = StyleSheet.create({
     },
     signInText: {
         fontFamily: 'montserrat',
-        fontSize: 16,
+        fontSize: 20,
         fontWeight: 'bold',
         color: '#4A2C1A',
         textAlign: 'center'
     },
     registerText: {
         fontFamily: 'montserrat',
-        fontSize: 16,
+        fontSize: 20,
         fontWeight: 'bold',
         color: '#FFFFFF',
         textAlign: 'center'
